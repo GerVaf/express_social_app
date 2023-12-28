77ada2104c50da156263cbffed302bff541f7f0b
GerVaf <thantzinmin667@gmail.com>
2023-12-28 21:52:37 +0630

    v1

--- controller/blogController.js
+++ controller/blogController.js
@@ -5,10 +5,26 @@ const { tryCatch } = require("../utils/tryCatch");
 
 const getBlogCollection = async () => await getDb().collection("blog");
 const getUserCollection = async () => await getDb().collection("user");
+const getCommentCollection = async () => await getDb().collection("comment");
 
 exports.getBlog = tryCatch(async (req, res) => {
-  const collection = await getBlogCollection();
-  const result = await collection.find({}).toArray();
+  const blogCollection = await getBlogCollection();
+
+  // Use aggregation with $lookup to join comments
+  const result = await blogCollection
+    .aggregate([
+      {
+        $lookup: {
+          from: "comment",
+          localField: "_id",
+          foreignField: "blogId",
+          as: "comments",
+        },
+      },
+      { $project: { "comments.blogId": 0, "comments._id": 0 } },
+    ])
+    .toArray();
+
   res.status(200).json({ data: result });
 });
 
@@ -33,13 +49,14 @@ exports.getBlog = tryCatch(async (req, res) => {
 exports.getOwnerBlog = tryCatch(async (req, res) => {
   const userCollection = await getUserCollection();
   const { ownerId } = req.body;
-  // console.log(ownerId)
+
   if (!ObjectId.isValid(ownerId)) {
     throw new BadRequest("Owner ID is required");
   }
+
   const blogOwner = new ObjectId(ownerId);
 
-  // search for blog owner
+  // Search for blog owner and their blogs including comments
   const result = await userCollection
     .aggregate([
       { $match: { _id: blogOwner } },
@@ -49,13 +66,29 @@ exports.getOwnerBlog = tryCatch(async (req, res) => {
           localField: "_id",
           foreignField: "blogOwner",
           as: "blogs",
+          pipeline: [
+            {
+              $lookup: {
+                from: "comment",
+                localField: "_id",
+                foreignField: "blogId",
+                as: "comments",
+              },
+            },
+            {
+              $project: {
+                "comments.blogId": 0,
+                "comments._id": 0,
+              },
+            },
+          ],
         },
       },
       { $project: { blogOwner: 0, info: 0 } },
     ])
     .toArray();
 
-  res.status(200).json({ message: "Here's owner's bolgs!!", data: result });
+  res.status(200).json({ message: "Here's the owner's blogs!", data: result });
 });
 
 exports.getBlogOwner = tryCatch(async (req, res) => {
@@ -99,4 +132,69 @@ exports.deleteBlog = tryCatch(async (req, res) => {
       .status(200)
       .json({ message: "Blog Deleted Successfully", data: result });
   }
+});
+
+exports.likeBlog = tryCatch(async (req, res) => {
+  const blogCollection = await getBlogCollection();
+
+  const { blogId } = req.body;
+  const userId = req.activeId;
+
+  if (!ObjectId.isValid(blogId))
+    throw new BadRequest("This user is not exist!!");
+  // Check if userId already exists in the like array
+  const blog = await blogCollection.findOne({
+    _id: new ObjectId(blogId),
+    like: userId,
+  });
+
+  let updateResult;
+  if (blog) {
+    // If userId exists in the like array, remove it
+    updateResult = await blogCollection.updateOne(
+      { _id: new ObjectId(blogId) },
+      { $pull: { like: userId } } // Removes userId from the like array
+    );
+  } else {
+    // If userId does not exist in the like array, add it
+    updateResult = await blogCollection.updateOne(
+      { _id: new ObjectId(blogId) },
+      { $addToSet: { like: userId } } // Adds userId to the like array, only if it's not already present
+    );
+  }
+
+  // You can then respond based on the update result
+  if (updateResult.modifiedCount === 1) {
+    res.status(201).json({ message: "Blog like updated successfully!" });
+  } else {
+    res.status(500).json({ message: "Unable to update the post." });
+  }
+});
+
+exports.getComment = tryCatch(async (req, res) => {
+  const commentCollection = await getCommentCollection();
+  const result = await commentCollection.find({}).toArray();
+  res.status(200).json({ message: "Here's comment!", data: result });
+});
+
+exports.commentBlog = tryCatch(async (req, res) => {
+  const commentCollection = await getCommentCollection();
+
+  const { blogId, userComment } = req.body;
+  const userId = new ObjectId(req.activeId);
+
+  // console.log(blogId, userComment);
+
+  const commentResult = await commentCollection.insertOne({
+    blogId: new ObjectId(blogId),
+    userId,
+    userComment,
+  });
+
+  console.log(commentResult);
+
+  res.status(201).json({
+    message: `You comment on ${blogId} this post`,
+    data: commentResult,
+  });
 });
--- router/blogRouter.js
+++ router/blogRouter.js
@@ -5,6 +5,9 @@ const {
   deleteBlog,
   getOwnerBlog,
   getBlogOwner,
+  likeBlog,
+  commentBlog,
+  getComment,
 } = require("../controller/blogController");
 const { authorize } = require("../middleware/authorize");
 const {
@@ -21,5 +24,8 @@ blogRouter
   .route("/create")
   .post(authorize, multerUploadMiddleware, cloudUploadMiddleware, createBlog);
 blogRouter.route("/delete/:id").delete(deleteBlog);
+blogRouter.route("/like").post(authorize,likeBlog)
+blogRouter.route("/comment").get(getComment)
+blogRouter.route("/comment").post(commentBlog)
 
 module.exports = blogRouter;
